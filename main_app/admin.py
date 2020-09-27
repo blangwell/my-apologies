40d5f5e94e93eaf8b77b41a4d2df75163d886cff
blangwell <b.langwell@outlook.com>
2020-09-27 14:29:51 -0700

    [admin] Create custom Accounts admin panel, Add main_app/urls.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from main_app.models import Account
 
 # Register your models here.
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+  list_display = ('email', 'display_name', 'date_joined', 'last_login', 'is_admin', 'is_staff')
+  search_fields = ('email', 'tags')
+  readonly_fields = ('date_joined', 'last_login')
+
+  filter_horizontal = ()
+  list_filter = ()
+  fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
--- main_app/models.py
+++ main_app/models.py
@@ -60,9 +60,7 @@ class Account(AbstractBaseUser):
   objects = MyAccountManager()
 
   def __str__(self):
-    return(
-      f'{self.email} has the display_name {self.display_name}'
-    )
+    return self.email
 
   def has_perm(self, perm, obj=None):
     return self.is_admin

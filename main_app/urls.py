cdab06aa9c77c4370b66585fb2ced227c607a84a
blangwell <b.langwell@outlook.com>
2020-09-29 09:45:40 -0700

    [urls] Move urlpatterns to the correct urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
 from . import views
 
+from main_app.views import (
+    registration_view, 
+    login_view, 
+    logout_view,
+    about,
+    profile,
+    change_password,
+)
+
+
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('register/', registration_view, name='register'),
+  path('login/', login_view, name='login'),
+  path('logout/', logout_view, name='logout'),
+  path('about/', about, name='about'),
+  path('account/<email>', profile, name='profile'),
+  path('password/', change_password, name='change_password')
 ]

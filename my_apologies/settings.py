08860de55200bab1ed9b1fa48631528384457665
blangwell <b.langwell@outlook.com>
2020-10-01 14:26:01 -0700

    [settings] Edit ALLOWED_HOSTS

--- my_apologies/settings.py
+++ my_apologies/settings.py
@@ -54,7 +54,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['my-apologies.herokuapp.com']
+ALLOWED_HOSTS = ['my-apologies.herokuapp.com',]
 
 
 # Application definition

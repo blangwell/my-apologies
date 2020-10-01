65e298626fe1135e96e170634869cef478a9f450
blangwell <b.langwell@outlook.com>
2020-10-01 10:57:26 -0700

    [deploy] Edit STATIC_ROOT in settings.py

--- my_apologies/settings.py
+++ my_apologies/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIR = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

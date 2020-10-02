a6276dea5ed39d5d9309aba7f809cf7918c78e0c
blangwell <b.langwell@outlook.com>
2020-10-01 19:45:01 -0700

    [settings] Debug static files issue. Save before test in production

--- my_apologies/settings.py
+++ my_apologies/settings.py
@@ -47,7 +47,7 @@ STATIC_URL = 'https://my-apologies.herokuapp.com/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
